22f16bc6081b79feb5e86755f6d630a2a15382fc
rachelpairol <rachelpairol@gmail.com>
2026-03-24 16:28:17 -0400

    Update logo

--- components/header.tsx
+++ components/header.tsx
@@ -15,7 +15,7 @@ export function Header() {
         <Link href="/" className="flex items-center gap-3">
           <div className="relative h-14 w-14">
             <Image
-              src="/Images/WeMark.png"
+              src="/Images/WeMarkLogo.png"
               alt="WeMark"
               fill
               className="object-contain"
